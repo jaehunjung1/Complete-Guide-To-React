c63466ef9b1187514d0e023e5e7d6e94cf75b4ab
Jae Hun Jung <sharkmir1@snu.ac.kr>
2019-09-25 00:29:17 +0900

    added redux-thunk

---
+++ 5_blog_redux_thunk/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import PostList from "./components/PostList";
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <PostList/>
+    </div>
+  );
+};
+
+export default App;
