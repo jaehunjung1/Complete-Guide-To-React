c8f2faabbad8be9bbe7873094be6604afdc5324c
Jae Hun Jung <sharkmir1@snu.ac.kr>
2019-09-24 23:10:24 +0900

    added comments on redux

--- section13_songs/src/components/App.js
+++ section13_songs/src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import './App.css';
-import {selectSong} from "../actions";
 import SongList from "./SongList";
 import SongDetail from "./SongDetail";
 
--- section13_songs/src/components/SongList.js
+++ section13_songs/src/components/SongList.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 import {selectSong} from '../actions'
-import SongDetail from "./SongDetail";
 
 class SongList extends Component {
   renderList() {
@@ -33,12 +32,16 @@ class SongList extends Component {
   }
 }
 
+// songs(SongList.prop에서 쓸 이름) : state.songs(Store에 선언된 reducer 이름)
+// 이렇게 선언된 mapStateToProps를 connect로 wrap하면 알아서 props.song에 reducer가 처리해준 state들이 업데이트 됨.
 const mapStateToProps = (state) => {
   return {
     songs: state.songs
   };
 };
 
+// connect: mapStateToProps(전체 reducer store에서 내가 무엇을 받을 건지), selectSong(내가 쓰고자 하는 action creator 이름)
+// 여기 등록된 것들은 SongList.prop, 즉 component.prop으로 reference 할 수 있게 됨.
 export default connect(mapStateToProps, {
   selectSong // same as selectSong: selectSong
 })(SongList);
