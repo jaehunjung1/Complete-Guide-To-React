3410aac731aa94e5f9282951bbd22409f0dbb4dd
Jae Hun Jung <sharkmir1@snu.ac.kr>
2019-09-24 13:51:53 +0900

    added section13

---
+++ section13_songs/src/components/SongDetail.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+const SongDetail = (props) => {
+  const song = props.song;
+
+  if (!song) {
+    return (
+      <div>Select a song</div>
+    );
+  } else {
+    return (
+      <div>
+        <h3>Details for:</h3>
+        <p>Title: {song.title}
+          <br/>
+          Duration: {song.duration}
+        </p>
+      </div>
+    );
+  }
+
+};
+
+const mapStateToProps = (state) => {
+  return {
+    song: state.selectedSong
+  };
+};
+
+export default connect(mapStateToProps)(SongDetail)
